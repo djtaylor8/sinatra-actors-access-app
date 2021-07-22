6771ac9b19aa8ceec61c1e70b2364269fa237888
DJ Taylor <dtaylor88@gmail.com>
2021-07-22 09:40:45 -0700

    create actor form

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,19 @@
-class ActorsController < ApplicationController 
+class ActorsController < ApplicationController
+    
+    
+    get '/actor-signup' do
+        if logged_in?
+            @actor = current_user 
+            redirect '/actors/#{@actor.slug}'
+        else
+            @agents = Agent.all   
+            erb :'actors/create_actor'
+        end 
+    end
+
+    post '/actor-signup' do 
+
+    end
 
     get '/actor-login' do
         if logged_in?
@@ -8,20 +23,16 @@ class ActorsController < ApplicationController
         end 
     end
 
-    post '/actor-login' do 
-
+    post '/actor-login' do
+        @actor = Actor.find_by(:email => params[:email])
+        if @user && user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect "/actors/#{@actor.slug}"
+        else 
+            flash[:message] = "Sorry, the username or password do not match. Please try agin."
+            redirect '/actor-login'
+        end
     end
-
-    get '/actor-signup' do 
-        erb :'actors/create_user'
-    end
-
-    post '/actor-signup' do 
-
-    end
-
-
-
 
 
 end
